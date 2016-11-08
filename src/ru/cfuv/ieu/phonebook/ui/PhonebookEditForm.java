6d2ce117339ac2d9ed7af8e81c096fb0212d270f
Alexandr9719 <sanchello2009@gmail.com>
2016-11-08 19:44:47 +0300

    Добавлено форма создания/редактирования контакта

--- src/ru/cfuv/ieu/phonebook/ui/PhonebookEditForm.java
+++ src/ru/cfuv/ieu/phonebook/ui/PhonebookEditForm.java
@@ -6,6 +6,13 @@ import javax.swing.*;
 
 public class PhonebookEditForm extends JFrame {
     private JPanel expPanel;
+    private JPanel additionalFields;
+    private JPanel additionalFieldsContainer;
+    private JTextField number1;
+    private JPanel numberPanel;
+    private JTextField nameField;
+    private JButton добавитьНомерButton;
+    private JButton добавитьПолеButton;
 
     public PhonebookEditForm(PhonebookContact person) {
         this.getContentPane().add(expPanel);
