879baa93d0085b8edff98a8ff6c3199314fdbe72
Ilya "EngiN33R" Gavrikov <painttool@gmail.com>
2016-11-08 21:26:07 +0300

    Доработано добавление и удаление номеров, исправлен интерфейс

--- src/ru/cfuv/ieu/phonebook/ui/PhonebookEditForm.java
+++ src/ru/cfuv/ieu/phonebook/ui/PhonebookEditForm.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+@SuppressWarnings("unused")
 public class PhonebookEditForm extends JFrame {
     private final PhonebookContact contact;
 
@@ -19,51 +20,76 @@ public class PhonebookEditForm extends JFrame {
     private JTextField number1;
     private JButton addNumber;
     private JButton addField;
-    private JTextField textField1;
+    private java.util.List<JComponent[]> numbers = new ArrayList<>();
     private java.util.List<JComponent[]> addFields = new ArrayList<>();
     private int numberGridCounter = 1;
     private int fieldGridCounter = 0;
 
-    public PhonebookEditForm(PhonebookContact person) {
-        this.getContentPane().add(expPanel);
-        this.contact = person;
-
+    private void addFirstNumber() {
         GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 0;
         c.weightx = 1.0d;
         c.fill = 1;
+        c.insets.bottom = 2;
+        c.gridwidth = GridBagConstraints.REMAINDER;
         number1 = new JTextField();
         numberPanel.add(number1, c);
+    }
+
+    public PhonebookEditForm(PhonebookContact person) {
+        this.getContentPane().add(expPanel);
+        this.contact = person;
+
+        addFirstNumber();
 
         addNumber.addActionListener(new ActionListener() {
+            private void redrawList() {
+                numberPanel.removeAll();
+                addFirstNumber();
+                numberGridCounter = 1;
+                for (JComponent[] comps : numbers) {
+                    GridBagConstraints c = new GridBagConstraints();
+                    c.gridy = numberGridCounter;
+
+                    c.fill = 1;
+                    c.gridx = 0;
+                    c.weightx = 0.99d;
+                    c.insets.bottom = 2;
+                    numberPanel.add(comps[0], c);
+
+                    c.gridx = 1;
+                    c.weightx = 0.01d;
+                    c.insets.left = 5;
+                    numberPanel.add(comps[1], c);
+
+                    numberGridCounter++;
+                }
+                numberPanel.revalidate();
+                numberPanel.repaint();
+            }
+
             @Override
             public void actionPerformed(ActionEvent e) {
                 final JTextField numberField = new JTextField();
                 final JButton removeNumber = new JButton(
                         new ImageIcon("res/Delete.png"));
+                removeNumber.setBorderPainted(false);
+                removeNumber.setContentAreaFilled(false);
+                removeNumber.setPreferredSize(new Dimension(16, 16));
 
-                GridBagConstraints c = new GridBagConstraints();
-                c.gridy = numberGridCounter;
-                c.fill = 1;
-                c.gridx = 0;
-                c.weightx = 1.0d;
-                numberPanel.add(numberField, c);
-                c.gridx = 1;
-                c.weightx = 0.0d;
-                numberPanel.add(removeNumber, c);
-                numberPanel.revalidate();
-
-                numberGridCounter++;
+                JComponent[] row = new JComponent[]{numberField, removeNumber};
 
                 removeNumber.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        numberPanel.remove(numberField);
-                        numberPanel.remove(removeNumber);
-                        numberPanel.revalidate();
+                        numbers.remove(row);
+                        redrawList();
                     }
                 });
+                numbers.add(row);
+
+                redrawList();
             }
         });
 
@@ -74,10 +100,12 @@ public class PhonebookEditForm extends JFrame {
                 for (JComponent[] comps : addFields) {
                     GridBagConstraints c = new GridBagConstraints();
                     c.gridy = fieldGridCounter;
+                    c.insets.bottom = 2;
 
                     c.gridx = 0;
                     c.weightx = 0.2;
                     c.fill = 1;
+                    c.insets.right = 5;
                     additionalFieldsContainer.add(comps[0], c);
 
                     c.gridx = 1;
@@ -99,13 +127,15 @@ public class PhonebookEditForm extends JFrame {
                 JTextField name = new JTextField();
                 JTextField label = new JTextField();
                 JButton delete = new JButton(new ImageIcon("res/Delete.png"));
-                delete.setVisible(true);
+                delete.setBorderPainted(false);
+                delete.setContentAreaFilled(false);
+                delete.setPreferredSize(new Dimension(16, 16));
 
                 JComponent[] row = new JComponent[]{name, label, delete};
                 delete.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        System.out.println(addFields.remove(row));
+                        addFields.remove(row);
                         redrawList();
                     }
                 });
