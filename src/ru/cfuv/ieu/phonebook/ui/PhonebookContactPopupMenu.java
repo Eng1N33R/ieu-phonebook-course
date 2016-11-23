08868a28da375597e6aba88a80ecd19f63aa481c
Ilya "EngiN33R" Gavrikov <painttool@gmail.com>
2016-11-23 13:34:49 +0300

    Подключение контекстного меню к логике главного экрана

--- src/ru/cfuv/ieu/phonebook/ui/PhonebookContactPopupMenu.java
+++ src/ru/cfuv/ieu/phonebook/ui/PhonebookContactPopupMenu.java
@@ -1,22 +1,13 @@
 package ru.cfuv.ieu.phonebook.ui;
 
-import ru.cfuv.ieu.phonebook.PhonebookRepository;
 import ru.cfuv.ieu.phonebook.model.PhonebookContact;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class PhonebookContactPopupMenu extends JPopupMenu {
-    private final PhonebookRepository repo;
-    private final PhonebookContact contact;
-
-    public PhonebookContactPopupMenu(PhonebookRepository repo,
-                                     PhonebookContact c) {
-        this.repo = repo;
-        this.contact = c;
-
+    public PhonebookContactPopupMenu(PhonebookRoot root, PhonebookContact c) {
         JMenuItem item1 = new JMenuItem("Редактировать...");
         JMenuItem item2 = new JMenuItem("Дублировать");
         JMenuItem item3 = new JMenuItem("Удалить...");
@@ -24,18 +15,19 @@ public class PhonebookContactPopupMenu extends JPopupMenu {
         item1.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
-                PhonebookEditForm form = new PhonebookEditForm(repo, contact);
-                form.pack();
-                form.setSize(new Dimension(200, 200));
-                form.setVisible(true);
-                form.setLocationRelativeTo(null);
+                root.editContact(c);
             }
         });
-
+        item2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                root.copyContact(c);
+            }
+        });
         item3.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
-                delete(contact);
+                root.deleteContact(c);
             }
         });
 
@@ -43,14 +35,5 @@ public class PhonebookContactPopupMenu extends JPopupMenu {
         add(item2);
         add(new JSeparator());
         add(item3);
-    }
-
-    private void delete(PhonebookContact c) {
-        int dialogResult = JOptionPane.showConfirmDialog(this,
-                "Вы уверены, что хотите удалить контакт " + c.getName() + "?",
-                "Удалить контакт", JOptionPane.YES_NO_OPTION);
-        if (dialogResult == JOptionPane.YES_NO_OPTION) {
-            System.out.println("deleting");
-        }
     }
 }
